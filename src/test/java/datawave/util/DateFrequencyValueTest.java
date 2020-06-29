2bba8b7f603d72b8146f28c8a3c645c99914434f
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2020-06-29 15:48:04 -0400

    Correctled num days in August from 30 to 31 - It caused an error

--- src/test/java/datawave/util/DateFrequencyValueTest.java
+++ src/test/java/datawave/util/DateFrequencyValueTest.java
@@ -49,8 +49,35 @@ public class DateFrequencyValueTest {
             if (OrdinalDayOfYear.isLeapYear(year))
                 dateFrequencyUncompressed.put(year + "0229", Integer.MAX_VALUE);
             
-            dateFrequencyUncompressed.put(year + "1231", 1231);
-            dateFrequencyUncompressed.put(year + "0131", 31);
+            dateFrequencyUncompressed.put(year + "0129", 129);
+            dateFrequencyUncompressed.put(year + "0130", 130);
+            dateFrequencyUncompressed.put(year + "0131", 131);
+            dateFrequencyUncompressed.put(year + "0329", 329);
+            dateFrequencyUncompressed.put(year + "0330", 330);
+            dateFrequencyUncompressed.put(year + "0331", 331);
+            dateFrequencyUncompressed.put(year + "0429", 429);
+            dateFrequencyUncompressed.put(year + "0430", 430);
+            dateFrequencyUncompressed.put(year + "0529", 529);
+            dateFrequencyUncompressed.put(year + "0530", 530);
+            dateFrequencyUncompressed.put(year + "0531", 531);
+            dateFrequencyUncompressed.put(year + "0629", 629);
+            dateFrequencyUncompressed.put(year + "0630", 630);
+            dateFrequencyUncompressed.put(year + "0729", 729);
+            dateFrequencyUncompressed.put(year + "0730", 730);
+            dateFrequencyUncompressed.put(year + "0731", 731);
+            dateFrequencyUncompressed.put(year + "0829", 829);
+            dateFrequencyUncompressed.put(year + "0830", 830);
+            dateFrequencyUncompressed.put(year + "0831", 831);
+            dateFrequencyUncompressed.put(year + "0929", 929);
+            dateFrequencyUncompressed.put(year + "0930", 930);
+            dateFrequencyUncompressed.put(year + "1029", 1029);
+            dateFrequencyUncompressed.put(year + "1030", 1030);
+            dateFrequencyUncompressed.put(year + "1031", 1031);
+            dateFrequencyUncompressed.put(year + "1129", 1129);
+            dateFrequencyUncompressed.put(year + "1130", 1130);
+            dateFrequencyUncompressed.put(year + "1229", 1229);
+            dateFrequencyUncompressed.put(year + "1230", 1230);
+            // dateFrequencyUncompressed.put(year + "1231", 1231);
             
         }
         
@@ -70,8 +97,8 @@ public class DateFrequencyValueTest {
         }
         log.info("The restored size is " + restored.size());
         log.info("The size of the unprocessed frequency map is " + dateFrequencyUncompressed.size());
-        Assert.assertTrue(dateFrequencyUncompressed.size() == 3380);
-        Assert.assertTrue(restored.size() == 3380);
+        Assert.assertTrue(dateFrequencyUncompressed.size() == 3640);
+        Assert.assertTrue(restored.size() == 3640);
         
         // Verify accurate restoration
         for (Map.Entry<String,Integer> entry : dateFrequencyUncompressed.entrySet()) {
