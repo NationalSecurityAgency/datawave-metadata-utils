ef6d919f0f0523ff2fc84cb805427042e685a4d9
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2020-07-21 13:07:26 -0400

    Added script to insert data in the project

--- src/test/java/datawave/util/FrequencyFamilyCounterTest.java
+++ src/test/java/datawave/util/FrequencyFamilyCounterTest.java
@@ -4,13 +4,41 @@ import datawave.query.util.FrequencyFamilyCounter;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.TimeUnit;
+
+//TODO Get this to really work
 public class FrequencyFamilyCounterTest {
     
     private FrequencyFamilyCounter counter = new FrequencyFamilyCounter();
     
     @Test
-    public void testCounter() {
-        Assert.assertTrue(counter != null);
+    public void testFrequencyColumnCompression() {
+        ProcessBuilder builder = new ProcessBuilder("accumulo shell -u root -p secret --execute-file resources/frequencyColumnCompressionData.txt");
+        
+        Process process = null;
+        try {
+            process = builder.start();
+        } catch (IOException ioException) {
+            System.out.println("Could not start the Datawave Quickstart " + ioException.getMessage());
+        }
+        if (process != null) {
+            try {
+                process.waitFor(3, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                
+            }
+        } else {
+            System.out.println("Datawave Failed to start");
+            return;
+        }
+        
+        if (process != null && process.exitValue() != 0)
+            System.out.println("Datawave Failed to start");
+        
     }
     
 }
