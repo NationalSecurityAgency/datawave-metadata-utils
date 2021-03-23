0faa9487f88d77fabc8a235f72a87199f65cc2ef
Jeffrey L. Zeiberg <jzeiberg@gmail.com>
2021-03-23 14:08:30 -0400

    Working on getting TreeSet<YearMonthDay> out of IndexedDatesValue

--- src/main/java/datawave/query/util/IndexedDatesValue.java
+++ src/main/java/datawave/query/util/IndexedDatesValue.java
@@ -7,10 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
 public class IndexedDatesValue implements Comparable<IndexedDatesValue> {
     
@@ -18,7 +15,7 @@ public class IndexedDatesValue implements Comparable<IndexedDatesValue> {
     
     private YearMonthDay startDay;
     private TreeSet<YearMonthDay> indexedDatesSet = new TreeSet<>();
-    private BitSet indexedDatesBitSet;
+    private BitSet indexedDatesBitSet = null;
     public static final int YYMMDDSIZE = 8;
     public static final int COMPARE_FAILED = Integer.MAX_VALUE;
     
@@ -51,51 +48,65 @@ public class IndexedDatesValue implements Comparable<IndexedDatesValue> {
      * @return
      */
     public Value serialize() {
-        if (startDay == null) {
-            log.warn("Start date was not initialized before serialization");
-            startDay = indexedDatesSet.first();
-        }
         
-        if (indexedDatesSet.isEmpty())
-            return null;
-        
-        YearMonthDay firstDay = indexedDatesSet.first();
-        if (!firstDay.equals(startDay)) {
-            log.warn("First day in treeset should be the start day");
-            log.warn("Start day will now be initialized to firstDate in the sorted treeset");
-            startDay = firstDay;
-        }
-        
-        YearMonthDay lastDay = indexedDatesSet.last();
-        // TODO Do a better job estimating the size using the ordinals
-        // Estimate the span of dates with firstDay and lastDay
-        int bitSetSize;
-        if (firstDay.equals(lastDay))
-            bitSetSize = 1;
-        else if (lastDay.getYear() == firstDay.getYear())
-            bitSetSize = lastDay.getJulian() - firstDay.getJulian() + 1;
-        else // Estimate the span of dates with firstDay and lastDay
-            bitSetSize = (lastDay.getYear() - firstDay.getYear() + 1) * 366;
-        indexedDatesBitSet = new BitSet(bitSetSize);
-        int dayIndex = 0;
-        YearMonthDay nextDay = startDay;
-        
-        for (YearMonthDay ymd : indexedDatesSet) {
-            if (ymd.compareTo(nextDay) == 0) {
-                indexedDatesBitSet.set(dayIndex);
-                dayIndex++;
-                nextDay = YearMonthDay.nextDay(nextDay.getYyyymmdd());
-            } else {
-                do {
-                    dayIndex++;
-                    nextDay = YearMonthDay.nextDay(nextDay.getYyyymmdd());
-                    if (ymd.compareTo(nextDay) == 0) {
-                        indexedDatesBitSet.set(dayIndex);
-                    }
-                    
-                } while (nextDay.compareTo(ymd) < 0);
+        try {
+            if (startDay == null) {
+                log.warn("Start date was not initialized before serialization");
+                startDay = indexedDatesSet.first();
             }
             
+        } catch (NoSuchElementException noSuchElementException) {
+            log.warn("No such element exception occurs when the indexedDateSet is not populated.");
+            log.warn("Default Empty IndexedDatesValue was created and never populated");
+            return new Value();
+        }
+        
+        // TODO Remove this try catch block and get the TreeSet<YearMonthDay> out of class
+        // The indexedDatesSet is used for testing the class so the test needs to change.
+        try {
+            YearMonthDay firstDay = indexedDatesSet.first();
+            
+            if (!firstDay.equals(startDay)) {
+                log.warn("First day in treeset should be the start day");
+                log.warn("Start day will now be initialized to firstDate in the sorted treeset");
+                startDay = firstDay;
+            }
+            
+            YearMonthDay lastDay = indexedDatesSet.last();
+            // TODO Do a better job estimating the size using the ordinals
+            // Estimate the span of dates with firstDay and lastDay
+            int bitSetSize;
+            if (firstDay.equals(lastDay))
+                bitSetSize = 1;
+            else if (lastDay.getYear() == firstDay.getYear())
+                bitSetSize = lastDay.getJulian() - firstDay.getJulian() + 1;
+            else // Estimate the span of dates with firstDay and lastDay
+                bitSetSize = (lastDay.getYear() - firstDay.getYear() + 1) * 366;
+            indexedDatesBitSet = new BitSet(bitSetSize);
+            int dayIndex = 0;
+            YearMonthDay nextDay = startDay;
+            
+            for (YearMonthDay ymd : indexedDatesSet) {
+                if (ymd.compareTo(nextDay) == 0) {
+                    indexedDatesBitSet.set(dayIndex);
+                    dayIndex++;
+                    nextDay = YearMonthDay.nextDay(nextDay.getYyyymmdd());
+                } else {
+                    do {
+                        dayIndex++;
+                        nextDay = YearMonthDay.nextDay(nextDay.getYyyymmdd());
+                        if (ymd.compareTo(nextDay) == 0) {
+                            indexedDatesBitSet.set(dayIndex);
+                        }
+                        
+                    } while (nextDay.compareTo(ymd) < 0);
+                }
+                
+            }
+        } catch (NoSuchElementException noSuchElementException) {
+            log.warn("The empty contructor was called and bitset and start day never populated.");
+            if (indexedDatesBitSet == null)
+                return new Value();
         }
         
         ByteArrayOutputStream baos = new ByteArrayOutputStream(YYMMDDSIZE + indexedDatesBitSet.size());
@@ -113,6 +124,39 @@ public class IndexedDatesValue implements Comparable<IndexedDatesValue> {
         }
         
         return new Value(baos.toByteArray());
+    }
+    
+    public static IndexedDatesValue deserialize(byte[] accumuloValue) {
+        
+        if (accumuloValue == null)
+            return null;
+        
+        String yyyyMMdd = new String(Arrays.copyOfRange(accumuloValue, 0, YYMMDDSIZE), StandardCharsets.UTF_8);
+        YearMonthDay startDay;
+        try {
+            startDay = new YearMonthDay(yyyyMMdd);
+        } catch (Exception e) {
+            log.warn("Accumulo value was not originally an IndexedDatesValue - Start date was not deserialized");
+            return new IndexedDatesValue();
+        }
+        IndexedDatesValue indexedDates = new IndexedDatesValue(startDay);
+        TreeSet<YearMonthDay> indexedDatesSet = new TreeSet<>();
+        indexedDatesSet.add(startDay);
+        YearMonthDay nextday = startDay;
+        if (accumuloValue.length > 8) {
+            BitSet theIndexedDates = BitSet.valueOf(Arrays.copyOfRange(accumuloValue, YYMMDDSIZE, accumuloValue.length));
+            indexedDates.setIndexedDatesBitSet(theIndexedDates);
+            for (int i = 1; i < theIndexedDates.size(); i++) {
+                nextday = YearMonthDay.nextDay(nextday.getYyyymmdd());
+                if (theIndexedDates.get(i)) {
+                    indexedDatesSet.add(nextday);
+                }
+            }
+            indexedDates.setIndexedDatesSet(indexedDatesSet);
+            log.info("The number of dates is " + indexedDatesSet.size());
+        }
+        
+        return indexedDates;
     }
     
     public static IndexedDatesValue deserialize(Value accumuloValue) {
