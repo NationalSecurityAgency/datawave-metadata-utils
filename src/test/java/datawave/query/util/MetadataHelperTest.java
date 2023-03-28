37422444c25e510c777910aa17c6df45c40c862e
Miner <andrew.lf.miner@gmail.com>
2023-03-28 17:52:19 +0000

    Organizing for more unit testing

--- src/test/java/datawave/query/util/MetadataHelperTest.java
+++ src/test/java/datawave/query/util/MetadataHelperTest.java
@@ -12,7 +12,6 @@ import org.apache.accumulo.core.client.BatchWriterConfig;
 import org.apache.accumulo.core.client.MutationsRejectedException;
 import org.apache.accumulo.core.client.TableExistsException;
 import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
@@ -29,8 +28,9 @@ public class MetadataHelperTest {
     private static final String TABLE_METADATA = "testMetadataTable";
     private static final String[] AUTHS = {"FOO"};
     private static MetadataHelper mdh;
+    private static AccumuloClient accumuloClient;
     
-    private static AllFieldMetadataHelper createAllFieldMetadataHelper(AccumuloClient accumuloClient) {
+    private static AllFieldMetadataHelper createAllFieldMetadataHelper() {
         final Set<Authorizations> allMetadataAuths = Collections.emptySet();
         final Set<Authorizations> auths = Collections.singleton(new Authorizations(AUTHS));
         TypeMetadataHelper tmh = new TypeMetadataHelper(Maps.newHashMap(), allMetadataAuths, accumuloClient, TABLE_METADATA, auths, false);
@@ -39,19 +39,10 @@ public class MetadataHelperTest {
         return new AllFieldMetadataHelper(tmh, cmh, accumuloClient, TABLE_METADATA, auths, allMetadataAuths);
     }
     
-    private static void setupTable(AccumuloClient accumuloClient) throws AccumuloException, TableExistsException, AccumuloSecurityException {
-        TableOperations tableOperations = accumuloClient.tableOperations();
-        if (!tableOperations.exists(TABLE_METADATA)) {
-            tableOperations.create(TABLE_METADATA);
-        }
-    }
-    
-    private static void addFields(AccumuloClient accumuloClient) throws TableNotFoundException {
+    private static void addFields(Mutation m) throws TableNotFoundException {
         BatchWriterConfig config = new BatchWriterConfig();
         config.setMaxMemory(0);
         try (BatchWriter writer = accumuloClient.createBatchWriter(TABLE_METADATA, config)) {
-            Mutation m = new Mutation("rowA");
-            m.put("t", "dataTypeA", new Value("value"));
             writer.addMutation(m);
             writer.flush();
         } catch (MutationsRejectedException e) {
@@ -61,25 +52,26 @@ public class MetadataHelperTest {
     
     @Before
     public void setup() throws TableNotFoundException, AccumuloException, TableExistsException, AccumuloSecurityException {
-        AccumuloClient accumuloClient = new InMemoryAccumuloClient("root", new InMemoryInstance(MetadataHelperTest.class.toString()));
-        setupTable(accumuloClient);
-        addFields(accumuloClient);
-        mdh = new MetadataHelper(createAllFieldMetadataHelper(accumuloClient), Collections.emptySet(), accumuloClient, TABLE_METADATA, Collections.emptySet(),
+        accumuloClient = new InMemoryAccumuloClient("root", new InMemoryInstance(MetadataHelperTest.class.toString()));
+        if (!accumuloClient.tableOperations().exists(TABLE_METADATA)) {
+            accumuloClient.tableOperations().create(TABLE_METADATA);
+        }
+        mdh = new MetadataHelper(createAllFieldMetadataHelper(), Collections.emptySet(), accumuloClient, TABLE_METADATA, Collections.emptySet(),
                         Collections.emptySet());
     }
     
     @Test // we expect our row to be filtered out
-    public void testPopulatedTypeFilter() throws TableNotFoundException {
-        assertEquals(Collections.EMPTY_SET, mdh.getAllFields(Collections.singleton("rowA")));
+    public void testSingleFieldFilter() throws TableNotFoundException {
+        Mutation m = new Mutation("rowA");
+        m.put("t", "dataTypeA", new Value("value"));
+        addFields(m);
+        
+        testFilter(Collections.EMPTY_SET, mdh.getAllFields(Collections.singleton("rowA")));
+        testFilter(Collections.singleton("rowA"), mdh.getAllFields(null));
+        testFilter(Collections.EMPTY_SET, mdh.getAllFields(Collections.EMPTY_SET));
     }
     
-    @Test // we expect a null filter to allow all the things
-    public void testNullTypeFilter() throws TableNotFoundException {
-        assertEquals(Collections.singleton("rowA"), mdh.getAllFields(null));
-    }
-    
-    @Test // we expect an empty filter to allow none of the things
-    public void testEmptyTypeFilter() throws TableNotFoundException {
-        assertEquals(Collections.EMPTY_SET, mdh.getAllFields(Collections.EMPTY_SET));
+    private void testFilter(Set<String> expected, Set<String> actual) throws TableNotFoundException {
+        assertEquals(expected, actual);
     }
 }
