05ce9804a6c2e6804bb2a04b82ba0fa6c3c8bc11
Miner <andrew.lf.miner@gmail.com>
2023-03-28 17:28:32 +0000

    Updating pom and formatting

--- src/test/java/datawave/query/util/MetadataHelperTest.java
+++ src/test/java/datawave/query/util/MetadataHelperTest.java
@@ -25,27 +25,27 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 
 public class MetadataHelperTest {
-
+    
     private static final String TABLE_METADATA = "testMetadataTable";
     private static final String[] AUTHS = {"FOO"};
     private static MetadataHelper mdh;
-
+    
     private static AllFieldMetadataHelper createAllFieldMetadataHelper(AccumuloClient accumuloClient) {
         final Set<Authorizations> allMetadataAuths = Collections.emptySet();
         final Set<Authorizations> auths = Collections.singleton(new Authorizations(AUTHS));
         TypeMetadataHelper tmh = new TypeMetadataHelper(Maps.newHashMap(), allMetadataAuths, accumuloClient, TABLE_METADATA, auths, false);
         CompositeMetadataHelper cmh = new CompositeMetadataHelper(accumuloClient, TABLE_METADATA, auths);
-
+        
         return new AllFieldMetadataHelper(tmh, cmh, accumuloClient, TABLE_METADATA, auths, allMetadataAuths);
     }
-
+    
     private static void setupTable(AccumuloClient accumuloClient) throws AccumuloException, TableExistsException, AccumuloSecurityException {
         TableOperations tableOperations = accumuloClient.tableOperations();
         if (!tableOperations.exists(TABLE_METADATA)) {
             tableOperations.create(TABLE_METADATA);
         }
     }
-
+    
     private static void addFields(AccumuloClient accumuloClient) throws TableNotFoundException {
         BatchWriterConfig config = new BatchWriterConfig();
         config.setMaxMemory(0);
@@ -58,25 +58,26 @@ public class MetadataHelperTest {
             throw new RuntimeException(e);
         }
     }
-
+    
     @Before
     public void setup() throws TableNotFoundException, AccumuloException, TableExistsException, AccumuloSecurityException {
         AccumuloClient accumuloClient = new InMemoryAccumuloClient("root", new InMemoryInstance(MetadataHelperTest.class.toString()));
         setupTable(accumuloClient);
         addFields(accumuloClient);
-        mdh = new MetadataHelper(createAllFieldMetadataHelper(accumuloClient), Collections.emptySet(), accumuloClient, TABLE_METADATA, Collections.emptySet(), Collections.emptySet());
+        mdh = new MetadataHelper(createAllFieldMetadataHelper(accumuloClient), Collections.emptySet(), accumuloClient, TABLE_METADATA, Collections.emptySet(),
+                        Collections.emptySet());
     }
-
+    
     @Test // we expect our row to be filtered out
     public void testPopulatedTypeFilter() throws TableNotFoundException {
         assertEquals(Collections.EMPTY_SET, mdh.getAllFields(Collections.singleton("rowA")));
     }
-
+    
     @Test // we expect a null filter to allow all the things
     public void testNullTypeFilter() throws TableNotFoundException {
         assertEquals(Collections.singleton("rowA"), mdh.getAllFields(null));
     }
-
+    
     @Test // we expect an empty filter to allow none of the things
     public void testEmptyTypeFilter() throws TableNotFoundException {
         assertEquals(Collections.EMPTY_SET, mdh.getAllFields(Collections.EMPTY_SET));
