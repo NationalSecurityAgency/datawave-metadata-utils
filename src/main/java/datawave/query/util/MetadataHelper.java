f3a874a2be23823d0594ecd2e8f69fc01cc1f864
Ivan Bella <ivan@bella.name>
2023-06-13 16:15:30 +0000

    Deprecated FieldMapping.isLenient() and added FieldMapping.isLenientMarker().

--- src/main/java/datawave/query/util/MetadataHelper.java
+++ src/main/java/datawave/query/util/MetadataHelper.java
@@ -432,7 +432,7 @@ public class MetadataHelper {
         for (Map.Entry<Key,Value> entry : scan) {
             try {
                 FieldMapping mapping = ModelKeyParser.parseKey(entry.getKey());
-                if (mapping.isLenient()) {
+                if (mapping.isLenientMarker()) {
                     queryModel.addLenientForwardMappings(mapping.getModelFieldName());
                 } else if (mapping.getDirection() == Direction.FORWARD) {
                     // Do not add a forward mapping entry
